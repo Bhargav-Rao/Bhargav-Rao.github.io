b69f00d88b3e0d04227d2b17ca3b20a50faa63b5
bhargav <bhargav.rao448@gmail.com>
2015-12-27 00:04:09 +0530

    stuff

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,16 @@ $(document).ready(function() {
   $('[data-toggle=offcanvas]').click(function() {
     $('.row-offcanvas').toggleClass('active');
   });
+  initialize();
 });
+
+
+function initialize() {
+  var mapProp = {
+    center:new google.maps.LatLng(12.928130, 77.573091),
+    zoom:18,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  google.maps.event.addDomListener(window, 'load', initialize);
+}
