83301622ad8510e8a44e4375db6ba09042ba2fef
bhargav <bhargav.rao448@gmail.com>
2015-12-28 01:29:11 +0530

    stuff

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,12 @@ function initialize() {
     zoom:18,
     mapTypeId:google.maps.MapTypeId.ROADMAP
   };
+
   var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+   var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(12.928130, 77.573091),
+    map: map,
+    title: 'My Home'
+  });
   google.maps.event.addDomListener(window, 'load', initialize);
 }
