3dc4399f3424a1e11c87863053865d412af1423b
Bhargav Rao <bhargav.rao448@gmail.com>
2020-06-01 13:31:58 -0700

    Delete AllFiles.js

--- Archive/js/AllFiles.js
+++
@@ -1,8 +0,0 @@
-            function init()
-            {
-                pg = document.getElementById('firstdiv');
-                pg.style.width =  window.innerWidth - 0.1 * window.innerHeight + "px";
-                pg.style.height = 0.9 * window.innerHeight + "px";
-                pg.style.left = 0.05 * window.innerHeight + "px";
-                pg.style.top = 0.05 * window.innerHeight + "px";
-            }
